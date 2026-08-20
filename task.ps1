e520f56c49e53a3ac0c2a645821fea441354215d
Alexey <alexey.zhuravel2004@gmail.com>
2026-08-20 17:18:19 +0200

    Sol4

--- task.ps1
+++ task.ps1
@@ -11,7 +11,8 @@ $publicIpAddressName = "linuxboxpip"
 $vmName = "matebox"
 $vmImage = "Ubuntu2204"
 $vmSize = "Standard_B2ats_v2"
-$dnsLabel = "matetask" + (Get-Random -Count 1) 
+$dnsLabel = "matetask" + (Get-Random -Count 1)
+$githubUsername = "Alexey211220"
 
 Write-Host "Creating a resource group $resourceGroupName ..."
 New-AzResourceGroup -Name $resourceGroupName -Location $location
@@ -48,9 +49,10 @@ $Params = @{
     Publisher          = 'Microsoft.Azure.Extensions'
     ExtensionType      = 'CustomScript'
     TypeHandlerVersion = '2.1'
+    Location           = $location
     ProtectedSettings  = @{
-        fileUris = @('https://raw.githubusercontent.com/Alexey211220/azure_task_12_deploy_app_with_vm_extention/develop/install-app.sh')
-        commandToExecute = 'bash install-app.sh'
+        fileUris = @('https://raw.githubusercontent.com/$githubUsername/azure_task_12_deploy_app_with_vm_extention/develop/install-app.sh')
+        commandToExecute = "GITHUB_USERNAME=$githubUsername bash install-app.sh"
     }
 }
 
